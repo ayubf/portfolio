7dc0db4385c33ed55c31ece3202f3284ee338e0c
ayubf <razortyphon@gmail.com>
2022-01-22 18:28:41 -0600

    Update Footer.tsx

--- src/components/subcomponents/Footer.tsx
+++ src/components/subcomponents/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <p>Designed and built by Ayub Farah, 2021</p>
             <p>Written in Python & TypeScript, using Flask & React</p>
             <div>
-                <a href="https://github.com/ayubf">Github</a> • <a href="https://www.linkedin.com/in/ayub-farah-15b278217/">Linkedin</a>
+                <a href="https://github.com/ayubf">Github</a> • <a href="https://www.linkedin.com/in/ayub-farah-15b278217/">Linkedin</a> • <a href="https://dev.to/ayubf">DEV</a>
             </div>
 	</div>
     )
