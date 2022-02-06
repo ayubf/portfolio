3cba96f4530ceec41c26b73236dc9f43f5bef180
ayubf <razortyphon@gmail.com>
2022-02-06 17:08:20 -0600

    Posts Delete

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import HomePage from "./components/HomePage";
 import {useState, useEffect} from 'react';
-import Post from "./components/Post";
-import Posts from "./components/Posts";
 import Projects from "./components/Projects";
 import Footer from "./components/subcomponents/Footer";
 import TopBar from "./components/subcomponents/TopBar";
@@ -39,15 +37,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<About />} />
-          <Route path="/posts" element={<Posts />} />
           <Route path="/projects" element={<Projects />} />
-          {
-              postData.map(({titleURL}) => {
-                return (
-                  <Route path={"/posts/"+titleURL+"/"} key={titleURL} element={<Post title={titleURL} />} />
-                )
-              })
-          }
         </Routes>
       </Router>
       <Footer />
--- src/components/subcomponents/TopBar.tsx
+++ src/components/subcomponents/TopBar.tsx
@@ -1,7 +1,7 @@
 const TopBar = () => {
     return(
         <div>
-            <a id='nameLink' href="/">Ayub Farah</a>  <a href="/about">About </a> - <a href="/projects"> Projects</a> - <a href="/posts">Blog</a>
+            <a id='nameLink' href="/">Ayub Farah</a>  <a href="/about">About </a> - <a href="/projects"> Projects</a>
         </div>
     )
 }
