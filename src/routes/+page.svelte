30a848ab46d4d7e3b90b8225c110a08bded9d538
Ayub Farah <ayubaf04@gmail.com>
2024-01-03 03:15:38 -0600

    New Portfolio

---
+++ src/routes/+page.svelte
@@ -0,0 +1,102 @@
+<style>
+
+    body {
+        max-width: max-content;
+        margin: auto;
+    }
+
+    @font-face {
+        font-family: "CMUSerifRoman";
+        src: url("../fonts/cmunrm.woff") format("woff");
+    }
+
+    .container {
+        font-family: "CMUSerifRoman";
+        width: 50vw;
+        height: 100%;
+        padding: 70px 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    a:visited {
+        color: blue;
+    }
+
+    a:hover {
+        color: rgb(98, 98, 240);
+    }
+
+</style>
+
+
+<body>
+
+    <div class="container">
+        <div>
+            <h1>Ayub Farah</h1>
+            <hr>
+        </div>
+        <div>
+            <h2>I. About</h2>
+            <p>
+                &emsp; My name is Ayub Farah. I'm currently a pursuing a Bachelors in Computer Science
+                at The University of Houston. I have an interest in Computer Science and Mathematics, specifically these topics:
+            </p>
+            <ul>
+                <li>Web Development</li>
+                <li>Linear Algebra</li>
+                <li>Machine Learning</li>
+                <li>Data Science</li>
+                <li>Geometry</li>
+                <li>Optimization</li>
+            </ul>
+        </div>
+        <div>
+            <h2>II. Experience</h2>
+            <div>
+                <div>
+                    <p>Amazon Web Services | SDE Intern</p>
+                    <p><i>June 2023 - August 2023</i></p>
+                    <hr>
+                </div>
+                <ul>
+                    <li>Developed a resource viewer application utilizing AWS Lambda & DynamoDB, enhancing accessibility and efficiency for the engineering team's resource management. </li>
+                    <li>Implemented role-based access control using AWS IAM, ensuring secure and streamlined access for team members within the resource viewer application. </li>
+                    <li>Enhanced the team's ticket management application with React and Python, integrating crucial edit functionalities to streamline the ticketing process and improve team productivity.</li>
+                </ul>
+            </div>
+            <div>
+                <div>
+                    <p>Google | Computer Science Summer Institute (CSSI) Scholar</p>
+                    <p><i>July 2022 - August 2022</i></p>
+                    <hr>
+                </div>
+                <ul>
+                    <li>Participated in a 4-week intensive computer science summer program for high-achieving students</li>
+                    <li>Completed an introductory project-based HTML/CSS and JavaScript curriculum taught by Google engineers</li>
+                    <li>Attended product design, resume development, and software engineering interview workshops </li>
+                    <li>Delivered a collaborative final project presentation that included a live demonstration to Google employees and community leaders</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <h2>III. Projects</h2>
+            <ul>
+                <li><a href="https://github.com/ayubf/Swish-Analytics">Swish Analytics</a>: A Deep Dive Into NBA Statistics</li>
+                <li><a href="https://github.com/ayubf/turms">Turms</a>: An instant messaging service using the WebSocket API.</li>
+                <li><a href="https://github.com/ayubf/wink">Wink</a>: Full-stack social media webapp.</li>
+            </ul>
+            <p>More on my <a href="https://github.com/ayubf">Github profile.</a></p>
+        </div>
+        <div>
+            <h2>IV. Contact</h2>
+            <ul>
+                <li><a href="https://docs.google.com/document/d/1s_ZSxTAYZjTrnart0uc524cu6R3ECRraFxKcMJL7nCw">Resume</a></li>
+                <li><a href="https://linkedin.com/in/ayubfarah">Linkedin</a></li> 
+                <li><a href="mailto:ayubaf04@gmail.com">Email</a></li>
+            </ul>
+        </div>
+    </div>
+
+</body>
